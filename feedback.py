a45a8056ff9f69aebc44c5ee1310bb75e799750a
dev-tam <tamjid.mdh@gmail.com>
2017-12-08 22:43:18 +0600

    wrap text

--- feedback.py
+++ feedback.py
@@ -6,14 +6,20 @@ from tkinter import ttk
 
 class Feedback:
     def __init__(self, master):
+
         self.frame_header = ttk.Frame(master)
+        self.frame_header.pack()
+
         self.logo = PhotoImage(file='images/logo.png')
-        ttk.Label(self.frame_header, image=self.logo)
-        ttk.Label(self.frame_header, text="Thanks for Exploring!")
-        ttk.Label(self.frame_header, text="We are glad you chose Explore California for your recent adventure. "
-                  "Please tell us what you thought about the 'Desert to Sea' tour")
+        ttk.Label(self.frame_header, image=self.logo).grid(row=0, column=0, rowspan=2)
+        ttk.Label(self.frame_header, text="Thanks for Exploring!").grid(row=0, column=1)
+        ttk.Label(self.frame_header, wraplength=300,
+                text=("We are glad you chose Explore California for your recent adventure. "
+                  "Please tell us what you thought about the 'Desert to Sea' tour")).grid(row=1, column=1)
 
         self.frame_content = ttk.Frame(master)
+        self.frame_content.pack()
+
         ttk.Label(self.frame_content, text='Name:')
         ttk.Label(self.frame_content, text='Email:')
         ttk.Label(self.frame_content, text='Comments:')
@@ -21,7 +27,7 @@ class Feedback:
         self.entry_name = ttk.Entry(self.frame_content, width=24)
         self.entry_email = ttk.Entry(self.frame_content, width=24)
 
-        self.text_comments = Text(self.frame_content,width=50, height=10)
+        self.text_comments = Text(self.frame_content, width=50, height=10)
         ttk.Button(self.frame_content, text='Submit')
         ttk.Button(self.frame_content, text='Clear')
 
