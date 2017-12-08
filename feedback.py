3f889c51c7036aa12881ef0cb549b805075b3d8b
dev-tam <tamjid.mdh@gmail.com>
2017-12-09 02:50:04 +0600

    Button Events

--- feedback.py
+++ feedback.py
@@ -2,6 +2,7 @@
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 
 class Feedback:
@@ -20,16 +21,33 @@ class Feedback:
         self.frame_content = ttk.Frame(master)
         self.frame_content.pack()
 
-        ttk.Label(self.frame_content, text='Name:')
-        ttk.Label(self.frame_content, text='Email:')
-        ttk.Label(self.frame_content, text='Comments:')
+        ttk.Label(self.frame_content, text='Name:').grid(row=0,column=0, sticky='sw')
+        ttk.Label(self.frame_content, text='Email:').grid(row=0,column=1, sticky='sw')
+        ttk.Label(self.frame_content, text='Comments:').grid(row=2, column=0, sticky='sw')
 
         self.entry_name = ttk.Entry(self.frame_content, width=24)
         self.entry_email = ttk.Entry(self.frame_content, width=24)
-
         self.text_comments = Text(self.frame_content, width=50, height=10)
-        ttk.Button(self.frame_content, text='Submit')
-        ttk.Button(self.frame_content, text='Clear')
+
+        self.entry_name.grid(row=1, column=0)
+        self.entry_email.grid(row=1, column=1)
+        self.text_comments.grid(row=3, column=0, columnspan=2)
+
+        ttk.Button(self.frame_content, text='Submit', command=self.submit).grid(row=4, column=0, sticky='e')
+        ttk.Button(self.frame_content, text='Clear', command=self.clear).grid(row=4, column=1, sticky='w')
+
+    def submit(self):
+        print('Name: {}'.format(self.entry_name.get()))
+        print('Email: {}'.format(self.entry_email.get()))
+        print('Comments: {}'.format(self.text_comments.get(1.0, 'end')))
+        self.clear()
+        messagebox.showinfo(title="Explore California Feedback", message="Comments Submitted!")
+
+
+    def clear(self):
+        self.entry_name.delete(0, 'end')
+        self.entry_email.delete(0, 'end')
+        self.text_comments.delete(1.0, 'end')
 
 
 def main():
