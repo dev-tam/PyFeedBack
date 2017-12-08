29c60684ccf33f0fbfb5a420732317c98c222f0f
dev-tam <tamjid.mdh@gmail.com>
2017-12-08 21:51:18 +0600

    Designing scratch.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jtam/Development/Projects/Python/Tkinter/venv"
+set -gx VIRTUAL_ENV "/home/jtam/Development/Projects/Python/Tkinter/FeebackApp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
